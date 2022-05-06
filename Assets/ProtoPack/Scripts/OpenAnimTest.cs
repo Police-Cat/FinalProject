ce91d0764f745c43678031468dfdde65e6ab9a28
maksim <maksim.ishakow@lanit-tercom.com>
2022-05-06 17:20:48 +0300

    Initial commit

---
+++ Assets/ProtoPack/Scripts/OpenAnimTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenAnimTest: MonoBehaviour {
+
+	Animator anim;
+	bool open = false;
+	void Start () {
+	
+		anim = GetComponent<Animator>();
+
+	}
+
+	void OnMouseDown(){
+
+		if (!open){
+
+			open = true;
+			anim.SetBool("open", open);
+
+		} else {
+
+			open = false;
+			anim.SetBool("open", open);
+
+
+		}
+
+	}
+
+}
